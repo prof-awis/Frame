346f015940d8d2b99a77f2dc1391a4a4269dc87e
Awino2000 <awinoemanuel2000@gmail.com>
2022-11-19 22:36:40 +0300

    Trying to work on the loop

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
-# Simple pygame program
-import frames
-
 # Import and initialize the pygame library
 import pygame
+
+from sys import exit
+
 pygame.init()
 
-# # importing os module 
-# import os 
-      
-# self.index = 0
-
-
+clock = pygame.time.Clock()
 # Set up the drawing window
 screen = pygame.display.set_mode([600, 700])
 
-images=[]
-frame = pygame.image.load("frames/1.png")
+# images=[]
+frame = pygame.image.load("frames/1.png").convert
+font = pygame.font.SysFont('arialblack', 20)
 
-    
+#the robot images  and the robot surface
+images = [
+ pygame.image.load("frames/1.png"),
+ pygame.image.load("frames/2.png"),
+ pygame.image.load("frames/3.png"),
+ pygame.image.load("frames/4.png"),
+ pygame.image.load("frames/5.png"),
+ pygame.image.load("frames/6.png"),
+ pygame.image.load("frames/7.png"),
+ pygame.image.load("frames/8.png"),
+ pygame.image.load("frames/9.png")
+]
+robot_surface = images[0]
 
-# images = [
-#  pygame.image.load("frames/1.png"),
-#  pygame.image.load("frames/2.png"),
-#  pygame.image.load("frames/3.png"),
-#  pygame.image.load("frames/4.png"),
-#  pygame.image.load("frames/5.png"),
-#  pygame.image.load("frames/6.png"),
-#  pygame.image.load("frames/7.png"),
-#  pygame.image.load("frames/8.png"),
-#  pygame.image.load("frames/9.png")
-# ]
+#RObot image selection index
+index = 0
+run_animation = True
 
-# def animate(images, image_index):
-#     image_index = increase_index()
-#     images = frames[image_index]
-#     screen.blit(images, (0,0))
-x=0
-y=0
-screen.blit(frame, (x, y))
-
-pygame.display.flip()
 # Run until the user asks to quit
 running = True
 while running:
@@ -49,14 +40,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the background with white
+    # Fill the background with color and add the images
     screen.fill((255, 255, 255))
-    # screen.blit(frame, (0, 0))
+    screen.blit(images, (0,332))
     
-
-
-    # Flip the display
+    # Drawing the buttonn and text written on it
+    button = pygame.draw.rect(window, 'blue', (10, 10, 120, 50), 0, 10)
+    draw_text('Animate', font, 'white', 23, 18)
     
+    #Get mouse position
+    mouse_pos = pygame.mouse.get_pos()
+    
+    #Check for collison of mouse with button
+    #if button.collidepoint(mouse_pos)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP and button.collidepoint(event.pos):
+            #run_animation = TRUE
+            print('animation true')
 
 # Done! Time to quit.
 pygame.quit()
